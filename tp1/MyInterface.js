b44d091cef1ead1e6868b018d633f5d441065e91
IlahaRahman <ilahe.rahmanzadeh@icloud.com>
2024-03-04 09:52:12 +0000

    tp1-1

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -19,9 +19,15 @@ export class MyInterface extends CGFinterface {
 
         //Checkbox element in GUI
         this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene, 'displayTriangle').name('Display Triangle');
+        this.gui.add(this.scene, 'displayParalellogram').name('Display Paralellogram');
+        this.gui.add(this.scene, 'displayTriangleSmall').name('Display TriangleSmall');
+        this.gui.add(this.scene, 'displayTriangleBig').name('Display TriangleBig');
 
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+       // this.gui.add(this.scene, 'diamondVisibility').name('Diamond Visibility');
+       // this.gui.add(this.scene, 'triangleVisibility').name('Triangle Visibility');
 
         return true;
     }
